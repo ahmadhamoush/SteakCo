535243ea7311d09b0a25f350a98a67cc39954afa
Ahmad Hamoush <hamoush@outlook.com>
2022-11-09 21:32:33 +0200

    Fixed small bug

--- scripts/animation.js
+++ scripts/animation.js
@@ -18,30 +18,55 @@ document.addEventListener('scroll', function() {
   }
 });
 
-document.addEventListener('scroll', function() {
-   document.querySelectorAll('.review-container').forEach(item=>{
-     var top = window.pageYOffset + window.innerHeight;
-       var isVisible = top > item.offsetTop;
-       if (isVisible) {
-          item.classList.add('review-active');
-       } else {
-            item.classList.remove('review-active');
-       }
-   })
+animate('.view-menu', 'transform');
+animate('.book', 'transform');
+animate('.reviews-title','fade');
+animate('.contact .inputs', 'fade');
+animate('.contact .message', 'transform');
+animateForEach('.review-container', 'review-active');
+animateForEach('.links .link', 'changeBg')
+animateMultiple('.why-us', ['.img-container','.why-us-body'],['transform2','slideLeft']);
+animateMultiple('.book', ['.contact span','.contact h2'],['slideLeft','slideRight']);
 
-});
 
-document.addEventListener('scroll', function() {
-  var top = window.pageYOffset + window.innerHeight;
-  var isVisible = top > document.querySelector('.why-us-body').offsetTop;
-  if (isVisible) {
-    document.querySelector('.img-container').classList.add('img-active');
-  } else {
-      document.querySelector('.img-container').classList.remove('img-active');
-  }
+function animate(item,classToAdd){
+  document.addEventListener('scroll', function() {
+    var top = window.pageYOffset + window.innerHeight;
+    var isVisible = top > document.querySelector(item).offsetTop;
+    if (isVisible) {
+          document.querySelector(item).classList.add(classToAdd);
+    } else {
+        document.querySelector(item).classList.remove(classToAdd);
+    }
+  });
+}
+function animateForEach(itemToAnimate, classToAdd){
+  document.addEventListener('scroll', function() {
+  document.querySelectorAll(itemToAnimate).forEach(item=>{
+    var top = window.pageYOffset + window.innerHeight;
+      var isVisible = top > item.offsetTop;
+      if (isVisible) {
+         item.classList.add(classToAdd);
+      } else {
+           item.classList.remove(classToAdd);
+      }
+  });
+  });
+}
 
-});
-
+function animateMultiple(view, items,classes){
+  document.addEventListener('scroll', function() {
+    var top = window.pageYOffset + window.innerHeight;
+    var isVisible = top > document.querySelector(view).offsetTop;
+    if (isVisible) {
+        document.querySelector(items[0]).classList.add(classes[0]);
+          document.querySelector(items[1]).classList.add(classes[1]);
+    } else {
+        document.querySelector(items[0]).classList.remove(classes[0]);
+        document.querySelector(items[1]).classList.remove(classes[1]);
+    }
+  });
+}
 function changeIcon() {
   if (document.getElementById('check').checked == true) {
     document.querySelector('label i').classList.remove('fa-bars');
